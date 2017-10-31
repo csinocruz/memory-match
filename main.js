dfe566fc1f053c35ed6e54b9619e55e8c86abb12
Cesca <csinocruz@gmail.com>
2017-10-31 16:36:11 -0700

    added theme music

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     console.log('document loaded'),
     setBackground()
+    // playMusic()
 });
 
 var background_images = [
@@ -21,6 +22,37 @@ var background_images = [
     '/Users/francescasinocruz/Desktop/LFZ/memory_match/images/Maps/volskaya_industries.jpg',
     '/Users/francescasinocruz/Desktop/LFZ/memory_match/images/Maps/watchpoint_gibraltar.jpg'
 ]
+
+// var theme_music = new Audio(
+//     '/Users/francescasinocruz/Desktop/LFZ/memory_match/music/637.mp3',
+//     '/Users/francescasinocruz/Desktop/LFZ/memory_match/music/1211.mp3',
+//     '/Users/francescasinocruz/Desktop/LFZ/memory_match/music/4732.mp3'
+// );
+
+// function playMusic() {
+//     console.log('playMusic function called');
+//     theme_music.loop = true;
+//     theme_music.play();
+// }
+
+var audio = new Audio(),
+i = 0;
+var playlist = new Array(
+    '/Users/francescasinocruz/Desktop/LFZ/memory_match/music/637.mp3', 
+    '/Users/francescasinocruz/Desktop/LFZ/memory_match/music/1211.mp3',
+    '/Users/francescasinocruz/Desktop/LFZ/memory_match/music/4732.mp3'
+);
+
+audio.addEventListener('ended', function () {
+i = ++i < playlist.length ? i : 0;
+console.log(i)
+audio.src = playlist[i];
+audio.play();
+}, true);
+audio.volume = 0.3;
+audio.loop = false;
+audio.src = playlist[0];
+audio.play();
 
 function setBackground() {
     console.log('function setBackground is being called');
